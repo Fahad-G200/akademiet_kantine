aaf15fc2fce28d85f78af399af931421a728d028
Fahad-G200 <fah_08@icloud.com>
2025-10-10 13:49:11 +0200

    Blokk 3: La til /meny

--- app.py
+++ app.py
@@ -10,6 +10,19 @@ app = Flask (__name__) # Lager appen
 def index():
     return "Hjem - Flask kjører! Prøv /meny, /varer eller /kontakt."
 
+# Blokk 3 - Legger til meny-siden
+@app.route("/meny")
+def meny():
+    return (
+        "Ukens meny:\n"
+        "Mandag: Pizza\n"
+        "Tirsdag: Taco\n"
+        "Onsdag: Pasta\n"
+        "Torsdag: Kylling\n"
+        "Fredag: Pizza"
+    )
+
+
 if __name__ == "__main__":
     app.run(debug=True) # Starter serveren
 
