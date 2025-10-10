e268257f364cb1b0ec741bada29eb328a3c5bd70
Fahad-G200 <fah_08@icloud.com>
2025-10-10 13:46:49 +0200

    Blokk 2: La til forsiden /

--- app.py
+++ app.py
@@ -5,6 +5,11 @@ from flask import Flask # importerer Flask
 
 app = Flask (__name__) # Lager appen
 
+# Blokk 2 - Legger til forsiden
+@app.route("/") 
+def index():
+    return "Hjem - Flask kjører! Prøv /meny, /varer eller /kontakt."
+
 if __name__ == "__main__":
     app.run(debug=True) # Starter serveren
 
