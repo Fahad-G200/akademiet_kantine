0e1c5afa2eeda6082e3594c0b6091ee80ed54d1d
Fahad-G200 <fah_08@icloud.com>
2025-10-10 13:18:53 +0200

    Gitacora saved 1 changed files

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
 # app.py
 # Første steg i kantinaprosjektet: minimal Flask-app
 # Jeg starter bare serveren og legger inn en rute ( forsiden) 
-from flask import Flask 
+from flask import Flask # importerer flask
